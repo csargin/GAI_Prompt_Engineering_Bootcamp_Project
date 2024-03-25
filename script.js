21bcdff397e9d48efd2285de94b7532c99ffe5e5
csargin <120469159+csargin@users.noreply.github.com>
2024-03-25 11:33:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
     var movies = [
         {title: 'The Shawshank Redemption', year: 1994, score: 9.3, image: 'movie1.jpg'},
         {title: 'The Godfather', year: 1972, score: 9.2, image: 'movie2.jpg'},
-        // Add the rest of the movies here...
+        {title: 'The Dark Knight', year: 2008, score: 9.01, image: 'movie2.jpg'},
+        {title: 'The Godfather Part II', year: 1974, score: 9.01, image: 'movie2.jpg'},
+        {title: '12 Angry Men', year: 1957, score: 9.01, image: 'movie2.jpg'},
+        {title: 'Schindlers List', year: 1993, score: 9.01, image: 'movie2.jpg'},
+        {title: 'The Lord of the Rings: The Return of the King', year: 2003, score: 9.01, image: 'movie2.jpg'},
+        {title: 'Pulp Fiction', year: 1994, score: 8.91, image: 'movie2.jpg'},
+        {title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001, score: 8.91, image: 'movie2.jpg'},
+
     ];
 
     var moviesSection = document.getElementById('movies');
@@ -31,8 +38,3 @@ document.addEventListener('DOMContentLoaded', function() {
         moviesSection.appendChild(movieDiv);
     });
 });
-
-
-
-
-
