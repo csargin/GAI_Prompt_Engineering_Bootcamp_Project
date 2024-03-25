edf2d047f1fe9a818e2e062d76d42febb7449520
csargin <120469159+csargin@users.noreply.github.com>
2024-03-25 11:47:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,49 +12,6 @@
 
     ];
 
-    var moviesSection = document.getElementById('movies');
-
-    movies.forEach(function(movie) {
-        var movieDiv = document.createElement('div');
-        movieDiv.className = 'movie';
-
-        var title = document.createElement('h2');
-        title.textContent = movie.title;
-
-        var year = document.createElement('p');
-        year.textContent = 'Year: ' + movie.year;
-
-        var score = document.createElement('p');
-        score.textContent = 'IMDb Score: ' + movie.score;
-
-        var image = document.createElement('img');
-        image.src = movie.image;
-
-        movieDiv.appendChild(title);
-        movieDiv.appendChild(year);
-        movieDiv.appendChild(score);
-        movieDiv.appendChild(image);
-
-        moviesSection.appendChild(movieDiv);
-    });
-});
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var movies = [
-        {title: 'The Shawshank Redemption', year: 1994, score: 9.3, image: 'movie1.jpg'},
-        {title: 'The Godfather', year: 1972, score: 9.2, image: 'movie2.jpg'},
-        // Add the rest of the movies here...
-    ];
-
     var moviesTable = document.getElementById('movies');
 
     for (var i = 0; i < movies.length; i++) {
